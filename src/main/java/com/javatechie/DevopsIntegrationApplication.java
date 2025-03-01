28f3b27c1b22461faaaa5eecbc42fb4eb7b0169b
zinebdri <zinebdrissi2004@gmail.com>
2025-03-01 02:22:25 +0000

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -3,24 +3,19 @@ package com.javatechie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DevopsIntegrationApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DevopsIntegrationApplication.class, args);
-    }
+	@GetMapping
+	public String message(){
+		return "welcome to devops-pipeline";
+	}
 
-    @GetMapping("/")
-    public String home(@RequestParam(defaultValue = "Zineb") String name) {
-        return "<html>" +
-                "<head><title>DevOps Pipeline</title></head>" +
-                "<body style='background-color: #282c34; color: white; text-align: center; padding: 50px;'>" +
-                "<h1>Bienvenue dans les pipeline DevOps, " + name + " 🚀</h1>" +
-                "</body>" +
-                "</html>";
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+	}
+
 }
